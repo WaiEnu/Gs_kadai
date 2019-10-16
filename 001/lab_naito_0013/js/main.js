c1ab451fa4fd32e6fa17fdfc8641992ac2465a71
ynaito <strayhituji@gmail.com>
2019-10-16 11:52:07 +0900

    19.10.16 ギャラリー表示修正

--- 001/lab_naito_0013/js/main.js
+++ 001/lab_naito_0013/js/main.js
@@ -33,4 +33,24 @@ $(function() {
     return false;
   });
 
+  $('.multiple').slick({
+    autoplay: true, //自動再生
+    infinite: true, //スライドのループ有効化
+    dots: true, //ドットのナビゲーションを表示
+    slidesToShow: 3, //表示するスライドの数
+    slidesToScroll: 4, //スクロールで切り替わるスライドの数
+    responsive: [{
+      breakpoint: 768, //ブレークポイントが768px
+      settings: {
+        slidesToShow: 3, //表示するスライドの数
+        slidesToScroll: 3, //スクロールで切り替わるスライドの数
+      }
+    }, {
+      breakpoint: 699, //ブレークポイントが480px
+      settings: {
+        slidesToShow: 1, //表示するスライドの数
+        slidesToScroll: 2, //スクロールで切り替わるスライドの数
+      }
+    }]
+  });
 });
