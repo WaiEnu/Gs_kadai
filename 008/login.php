4496495c6e33834a0176b2f6f4009aab4af1de58
WaiEnu <strayhituj@gmail.com>
2019-11-22 13:34:56 +0900

    19.11.22

--- 008/login.php
+++ 008/login.php
@@ -7,9 +7,13 @@
   <article>
     <div id="input">
       <form method="POST" post="admin.php">
-        <div>ID: <input type="text" name="lid"></div>
-        <div>PASS: <input type="text" name="pass"></div>
-        <button type="submit">ログイン</button>
+        <div class="jumbotron">
+          <fieldset>
+            <div>ID: <input type="text" name="lid"></div>
+            <div>PASS: <input type="text" name="pass"></div>
+            <div><input type="submit" value="ログイン"></div>
+          </fieldset>
+        </div>
       </form>
     </div>
   </article>
